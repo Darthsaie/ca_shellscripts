58e1a4f4d70a82b4125e1131e96b840175c9d5ac
Darthsaie <steven.nemtzoff@gmail.com>
2023-01-05 13:01:27 +0100

    MESSAGE DE COMMIT

--- array_comparison.sh
+++ array_comparison.sh
@@ -1,12 +1,21 @@
-#!/bin/bash
-NUMBERS=(951 402 984 651 360 69 408 319 601 485 980 507 725 547 544 615 83 165 141 501 263 617 865 575 219 390 237 412 566 826 248 866 950 626 949 687 217 815 67 104 58 512 24 892 894 767 553 81 379 843 831 445 742 717 958 609 842 451 688 753 854 685 93 857 440 380 126 721 328 753 470 743 527)
+# Declare arrays
+a=(3 5 8 10 6)
+b=(6 5 4 12)
+c=(14 7 5 7)
 
-# write your code here
-for gg in ${NUMBERS[@]}; do
-	
-    if [ $gg == 237 ]; then
-    	break;
-    elif [ $(($gg % 2)) == 0 ]; then
-    	echo $gg
-    fi
+# Initialize result array
+result=()
+
+# Loop through elements in array a
+for element in ${a[@]}
+do
+  # Check if element exists in arrays b and c
+  if [[ " ${b[@]} " =~ " ${element} " ]] && [[ " ${c[@]} " =~ " ${element} " ]]
+  then
+    # If element exists in both arrays b and c, add it to result array
+    result+=($element)
+  fi
 done
+
+# Print result array
+echo "${result[@]}"
